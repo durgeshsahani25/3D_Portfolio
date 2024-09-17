76f505d0ae98b7671f302a2e5e467f37f9b6618a
Durgesh <durgeshsahani6789@gmail.com>
2024-09-17 21:10:46 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@react-three/drei': 'path/to/@react-three/drei',
+      '@react-three/fiber': 'path/to/@react-three/fiber',
+    },
+  },
 });
